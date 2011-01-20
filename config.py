65900327aaddafaa99fb763957fcbf7c1d796e8d
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2011-01-20 02:20:10 -0500

    removed security information

--- config.py
+++ config.py
@@ -1,8 +1,30 @@
+# The set of extensions which the user is allowed to upload.
 ALLOWED_EXTENSIONS = set(('png', 'jpg', 'jpeg'))
-DATABASE = '/tmp/ophot.db'
-DEBUG = True
-PASSWORD = 'Ihtsp!!'
+
+# Never run a production server with DEBUG mode enabled.
+DEBUG = False
+
+# This is the location to which uploaded photos will be stored.
 PHOTO_DIR = 'static/photos'
+
+# This is the database schema.
 SCHEMA = 'schema.sql'
-SECRET_KEY = '\r\x93\xed\x14\x9c\x08\xa6\xf8\xb7{>(\x05\xfe\x15 \xbb\x95e%t\x01\xb7\xf5'
-USERNAME = 'admin'
+
+# The location to store the database for photos.
+DATABASE = 'db/ophot.db'
+
+#############################################
+# Add or modify all information below here. #
+#############################################
+
+# Generate a secret key to authenticate sessions with a logged in user.
+#SECRET_KEY = ''
+
+# Fill in the username and password for the user that will manage photos.
+#USERNAME = ''
+#PASSWORD = ''
+
+# Add your contact information.
+EMAIL = 'example@example.com'
+NAME = 'Examply McExample'
+PHONE = '555-5555'
