48139feb9b5ffe79b40283afeea0768af2b4a287
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2011-05-31 21:00:09 -0400

    update information in setup.py and remove unnecessary MANIFEST.in file

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup
 
 setup(
-    name='Ophot',
-    version='0.1',
-    long_description=__doc__,
-    packages=['ophot'],
+    author='Jeffrey Finkelstein',
+    author_email='jeffrey.finkelstein@gmail.com',
+    description='Photography portfolio website',
     include_package_data=True,
-    zip_safe=False,
     install_requires=['Flask', 'Flask-WTF', 'Flask-Uploads', 'PIL',
-                      'configobj']
+                      'configobj'],
+    keywords='photography website',
+    license='GNU AGPLv3',
+    long_description=__doc__,
+    name='Ophot',
+    packages=['ophot'],
+    setup_requires=['setuptools_hg'],
+    url='https://github.com/jfinkels/ophot',
+    version='0.1',
+    zip_safe=False
 )
